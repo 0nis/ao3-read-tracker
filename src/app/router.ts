5364f626bfa08cbc37604cba971b90b67b630360
Suzanne <82707067+0nis@users.noreply.github.com>
2026-05-01 10:33:10 +0200

    Revert router.ts to original
    u stupid, past me, the fix was already implemented! Dumbass smh

--- src/app/router.ts
+++ src/app/router.ts
@@ -11,18 +11,7 @@ export const Router = {
   },
 
   navigate(path: string) {
-    // TODO: Fix navigation issue
-    // okay I figured out why it's happening again and how to fix
-    // basically, future me, this is happening because instead of actually *navigating*, I'm *only* adding a new path to the url.
-    // that indeed does add it to the history and allows me to navigate "back" to the previous page, but...
-    // the previous page was fully overwritten by my "resolve". It does not exist anymore.
-    // to fix, do something like you did with the error page in bootstrap: navigate to "/", then replace that content.
-    // of course, also ensure you (visually!) update the URL just like in bootstrap.
-
-    // window.location.replace(FALLBACK_PATH); // <-- remember, your code stops running after this! Use SessionStorage to identify the original path
-    // history.replaceState({}, "", ORIGINAL_PATH); // <-- used to visually restore the original path
-    window.history.pushState({}, "", path);
-    this.resolve();
+    window.location.href = path;
   },
 
   back() {
@@ -34,7 +23,6 @@ export const Router = {
   },
 
   resolve() {
-    console.log("resolve", location.pathname);
     const match = this.routes.find((r) => r.path === location.pathname);
     if (match) match.render();
   },
